9525ccbe808e8015f1a257d855f58e218db4476e
Júlia Lambert <julialambert@MacBook-Pro-de-Julia.local>
2022-06-19 10:26:47 -0300

    feat: creating the expense list

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 import { 
   Container,
   Header,
@@ -10,10 +11,36 @@ import {
   UserGreeting,
   UserName,
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard(){
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 4.000,00',
+      category: {name: 'Vendas', icon: 'dollar-sign'},
+      date: '12/09/2022'
+    },
+    {
+      id: '2',
+      type: 'negative',
+      title: 'Aluguel',
+      amount: 'R$ 4.000,00',
+      category: {name: 'Aluguel', icon: 'coffee'},
+      date: '19/09/2022'
+    }
+  ]
+
   return (
     <Container>
       <Header>
@@ -48,6 +75,14 @@ export function Dashboard(){
           lastTransaction='16 de abril'
         />
       </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList 
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />  
+      </Transactions>      
     </Container>
   )
 }
