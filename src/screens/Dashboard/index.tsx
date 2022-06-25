d8b86b7ebb174763627f2a6ffa5192cb371d0d13
Júlia Lambert <julialambert@MacBook-Pro-de-Julia.local>
2022-06-25 12:38:36 -0300

    feat: adding asyncStorage

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
+import { useTheme } from 'styled-components';
 import { 
   Container,
   Header,
@@ -15,77 +19,178 @@ import {
   HighlightCards,
   Transactions,
   Title,
-  TransactionList
+  TransactionList,
+  LoadContainer
 } from './styles'
 
 export interface DataListProps extends TransactionCardProps {
   id: string;
 }
 
+interface HighLightProps {
+  amount: string;
+  lastTransaction: string;
+}
+
+interface HighLightData {
+  entries: HighLightProps;
+  expensives: HighLightProps;
+  total: HighLightProps;
+}
+
 export function Dashboard(){
-  const data: DataListProps[] = [
-    {
-      id: '1',
-      type: 'positive',
-      title: 'Desenvolvimento de site',
-      amount: 'R$ 4.000,00',
-      category: {name: 'Vendas', icon: 'dollar-sign'},
-      date: '12/09/2022'
-    },
-    {
-      id: '2',
-      type: 'negative',
-      title: 'Aluguel',
-      amount: 'R$ 4.000,00',
-      category: {name: 'Aluguel', icon: 'coffee'},
-      date: '19/09/2022'
-    }
-  ]
+  const [isLoading, setIsLoading] = useState(true);
+  const [transactions, setTransactions] = useState<DataListProps[]>([]);
+  const [highLightData, setHighLightData] = useState<HighLightData>({} as HighLightData);
+  const theme = useTheme()
+
+  function getLastTransactionDate(
+    collection : DataListProps[], 
+    type: 'positive' | 'negative'
+  ){
+    const lastTransaction = new Date(
+    Math.max.apply(Math, collection
+    .filter(transaction => transaction.type === type)
+    .map(transaction => new Date(transaction.date).getTime())));
+    
+    return `${lastTransaction.getDate()} de ${lastTransaction.toLocaleString('pt-BR', {
+      month: 'long'
+    })}`
+  }
+
+  async function loadTransactions() {
+    const dataKey = '@gofinances:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+    const transactions = response ? JSON.parse(response) : [];
+
+    let entriesTotal = 0;
+    let expensiveTotal= 0;
+
+    const transactionsFormatted: DataListProps[] = transactions
+      .map((item: DataListProps) => {
+
+        if(item.type === 'positive'){
+          entriesTotal += Number(item.amount);
+        } else {
+          expensiveTotal += Number(item.amount);
+        }
+        
+        const amount = Number(item.amount)
+        .toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        });
+
+        const date = Intl.DateTimeFormat('pt-BR', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit',
+        }).format(new Date(item.date));
+
+        return {
+          id: item.id,
+          name: item.name,
+          amount,
+          type: item.type,
+          category: item.category,
+          date,
+        }
+    });
+    setTransactions(transactionsFormatted);
+    const lastTransactionEntries =  getLastTransactionDate(transactions, 'positive');
+    const lastTransactionExpensives =  getLastTransactionDate(transactions, 'negative');
+    const totalInterval = `01 a ${lastTransactionExpensives}`;
+
+    const total = entriesTotal - expensiveTotal
+    setHighLightData({
+      entries: {
+        amount: entriesTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }),
+        lastTransaction: `Última entrada dia ${lastTransactionEntries}`,
+      },
+      expensives: {
+        amount: expensiveTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }),
+        lastTransaction: `Última saída dia ${lastTransactionExpensives}`,
+      },
+      total: {
+        amount: total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }),
+        lastTransaction: totalInterval
+      }
+    });
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  },[])
+
+  useFocusEffect(useCallback(() => {
+    loadTransactions();
+  },[]));
 
   return (
     <Container>
-      <Header>
-        <UserWrapper>
-          <UserInfo>
-            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/39964271?v=4'}}/>
-            <User>
-              <UserGreeting>Olá,</UserGreeting>
-              <UserName>Júlia</UserName>
-            </User>
-          </UserInfo>
-          <LogoutButton onPress={() => {}}>
-            <Icon name='power'/>
-          </LogoutButton>
-        </UserWrapper>  
-      </Header>
-      <HighlightCards>
-        <HighlightCard
-          type='up'
-          title='Entrada'
-          amount='R$14.000,00'
-          lastTransaction='13 de abril'
-        />
-        <HighlightCard 
-          type='down'
-          title='Saidas'
-          amount='R$4.000,00'
-          lastTransaction='14 de abril'
-        />
-        <HighlightCard
-          type='total'
-          title='Total'
-          amount='R$14.000,00'
-          lastTransaction='16 de abril'
-        />
-      </HighlightCards>
-      <Transactions>
-        <Title>Listagem</Title>
-        <TransactionList 
-          data={data}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => <TransactionCard data={item} />}
-        />  
-      </Transactions>      
+      {isLoading ? 
+        <LoadContainer>
+          <ActivityIndicator 
+            color={theme.colors.primary}
+            size='large'
+          />
+        </LoadContainer>
+      :
+        <>
+          <Header>
+            <UserWrapper>
+              <UserInfo>
+                <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/39964271?v=4'}}/>
+                <User>
+                  <UserGreeting>Olá,</UserGreeting>
+                  <UserName>Júlia</UserName>
+                </User>
+              </UserInfo>
+              <LogoutButton onPress={() => {}}>
+                <Icon name='power'/>
+              </LogoutButton>
+            </UserWrapper>  
+          </Header>
+          <HighlightCards>
+            <HighlightCard
+              type='up'
+              title='Entrada'
+              amount={highLightData.entries.amount}
+              lastTransaction={highLightData.entries.lastTransaction}
+            />
+            <HighlightCard 
+              type='down'
+              title='Saidas'
+              amount={highLightData.expensives.amount}
+              lastTransaction={highLightData.expensives.lastTransaction}
+            />
+            <HighlightCard
+              type='total'
+              title='Total'
+              amount={highLightData.total.amount}
+              lastTransaction={highLightData.total.lastTransaction}
+            />
+          </HighlightCards>
+          <Transactions>
+            <Title>Listagem</Title>
+            <TransactionList 
+              data={transactions}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => <TransactionCard data={item} />}
+            />  
+          </Transactions>
+        </>
+      }   
     </Container>
   )
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid'
 import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native'
 import { Button } from '../../components/Form/Button';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
@@ -40,15 +43,18 @@ export function Register(){
     name: 'Categoria',
   });
 
+  const navigation = useNavigation();
+
   const {
     control,
     handleSubmit,
+    reset,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
   });
 
-  function handleTransactionsTypesSelect(type: 'up' | 'down') {
+  function handleTransactionsTypesSelect(type: 'positive' | 'negative') {
     setTransactionType(type);
   }
 
@@ -60,18 +66,47 @@ export function Register(){
     setCategoryModalOpen(false);
   }
 
-  function handleRegister(form : FormData){
+  async function handleRegister(form : FormData){
     if(!transactionType)
       return Alert.alert('Selecione o tipo da transação');
 
     if(category.key === 'category')
       return Alert.alert('Selecione a categoria');
     
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
-      transactionType,
-      category: category.key
+      type: transactionType,
+      category: category.key,
+      date: new Date()
+    }
+    
+    try {
+      const dataKey = '@gofinances:transactions';
+
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ];
+      
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+      reset();
+      setTransactionType('');
+      setCategory({
+        key: 'category',
+        name: 'Categoria'
+      });
+
+      navigation.navigate('Listagem');
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possível salvar');
     }
   }
 
@@ -103,14 +138,14 @@ export function Register(){
               <TransactionTypeButton 
                 type='up'
                 title='Income'
-                onPress={() => handleTransactionsTypesSelect('up')}
-                isActive={transactionType === 'up'}
+                onPress={() => handleTransactionsTypesSelect('positive')}
+                isActive={transactionType === 'positive'}
               />
               <TransactionTypeButton 
                 type='down'
                 title='Outcome'
-                onPress={() => handleTransactionsTypesSelect('down')}
-                isActive={transactionType === 'down'}
+                onPress={() => handleTransactionsTypesSelect('negative')}
+                isActive={transactionType === 'negative'}
               />
             </TransactionsTypes>  
             <CategorySelectButton 
